f514715f18eafc75474c28f8c2fb4a0f324e53d0
dashan0313 <hjs349960325@163.com>
2020-10-19 16:15:47 -0500

    add new file

---
+++ leetcode/837_new21.py
@@ -0,0 +1,20 @@
+爱丽丝参与一个大致基于纸牌游戏 “21点” 规则的游戏，描述如下：
+
+爱丽丝以 0 分开始，并在她的得分少于 K 分时抽取数字。 抽取时，她从 [1, W] 的范围中随机获得一个整数作为分数进行累计，其中 W 是整数。 每次抽取都是独立的，其结果具有相同的概率。
+
+当爱丽丝获得不少于 K 分时，她就停止抽取数字。 爱丽丝的分数不超过 N 的概率是多少？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/new-21-game
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution:
+    def new21Game(self, N: int, K: int, W: int) -> float:
+        if K == 0:
+            return 1.0
+        dp = [0.0] * (K + W)
+        for i in range(K, min(N, K + W - 1) + 1):
+            dp[i] = 1.0
+        dp[K - 1] = float(min(N - K + 1, W)) / W
+        for i in range(K - 2, -1, -1):
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W
+        return dp[0]
---
+++ leetcode/sql/626.sql
@@ -0,0 +1,20 @@
+#换座位
+SELECT
+    (CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (SELECT
+        COUNT(*) AS counts
+    FROM
+        seat) AS seat_counts
+ORDER BY id ASC;
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/exchange-seats/solution/huan-zuo-wei-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
